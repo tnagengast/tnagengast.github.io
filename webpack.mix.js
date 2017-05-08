659139f0cfc300312eb67c04e8855c09cf6fd4c1
Thomas Nagengast <tnagengast@gmail.com>
2017-05-07 17:21:58 -0700

    Rip it all out

--- webpack.mix.js
+++
@@ -1,6 +0,0 @@
-const { mix } = require('laravel-mix');
-
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css')
-   .copy('node_modules/video.js/dist/video-js.min.css', 'public/css/video-js.min.css')
-   .browserSync('dev-portfolio.dev');
